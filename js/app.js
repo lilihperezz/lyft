4b41416e1363f80ffcc4bab714381f7dd936c172
lili hualtibamba <lilihperezz@gmail.com>
2016-10-18 22:31:16 -0500

    agregando modificaciones

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
+/*
 $(document).ready(function() {
-	$("#numeroCelular").keydown(valorTeclas);
-	$("#numeroCelular").keyup(longitudNumeros);
-	$("#btnSignup").click(numerosAleatorios);
-	$("#contenedorNumeroCel").text(localStorage.getItem("numeroMovistar"));
+	$("#ingresarNumero").keydown(valorTeclas);
+	$("#ingresarNumero").keyup(longitudNumeros);
+	$("#btn-signup").click(numerosAleatorios);
+	$("#btn-contactos").click(validarFormContacto);
+	$("#numero1").keydown(cambiarInput);
+	$("#contenedorNumeroCel").text(localStorage.getItem("numeroIngresado"));
+		if (navigator.geolocation) { 
+		// también se puede usar if ("geolocation" in navigator) {}
+		navigator.geolocation.getCurrentPosition(exito, error);
+	}
+	$("#numero1").focus();
+});
 
 function valorTeclas(evento) {
 		var ascii = evento.keyCode;
@@ -15,15 +24,24 @@ function valorTeclas(evento) {
 function longitudNumeros(evento) {
 		var longitud = $(this).val().length;
 		if (longitud == 9) {
-			$("#btnSignup").attr("href", "contactos.html");
+			$("#btnSignup").attr("href", "verify-phone.html");
 		} else {
 			$("#btnSignup").removeAttr("href");
 				
 		}
 	}
+function validarFormContacto(evento){
+	var firstName = $("#firstName").val().length;
+	var lastName = $("#lastName").val().length;
+		if( 20>=firstName && lastName >=2){
+			return true;
+		}else {
+		alert("Ingrese mínimo 2 y máximo 20 caracteres");
+	}
+}
 function numerosAleatorios(evento){
-	var valorNumero = $("#numeroCelular").val().length;
-		if (valorNumero == 9) {
+	var numeroIngresado = $("#ingresarNumero").val().length;
+		if (numeroIngresado == 9) {
 		//	$("#btnSignup[disabled]")= false;
 		var numeroRandom = Math.round(Math.random()*899)+100 ;
 		var codigoLab = "LAB-";
@@ -32,10 +50,54 @@ function numerosAleatorios(evento){
 		}else{
 			alert("ingrese 9 numeros");
 		}
-		localStorage.setItem("numeroMovistar",$("#numeroCelular").val());
+	localStorage.setItem("numeroIngresado",$("#ingresarNumero").val());
+	localStorage.setItem("codigoAleatorio",codigoLabNumero);
+}
+function cambiarInput(){
+	if($(this).val().length == 1){
+		$(this).next().focus();
 	}
+}
+function exito(posicion) {
+	var lat = posicion.coords.latitude;
+    var lon = posicion.coords.longitude;
+    var latlon = new google.maps.LatLng(lat, lon)
+ //   var mapa = document.getElementById('mapa')
+ 	$("#mapa").add("classMap")
+ //   mapa.style.height = '450px';
+ //   mapa.style.width = '300px';
 
-});
+    var myOptions = {
+	    center:latlon,zoom:14,
+	    mapTypeId:google.maps.MapTypeId.ROADMAP,
+	    mapTypeControl:true,
+	    navigationControlOptions:{
+	    	style: google.maps.NavigationControlStyle.SMALL
+	   	}
+    };
+    
+    var map = new google.maps.Map(document.getElementById('mapa'), myOptions);
+
+    var marker = new google.maps.Marker({
+    	position:latlon,
+    	map:map,
+    	title:"Estás aquí"
+    });
+};
+ function error(error) {
+	console.log(error);
+};
+
+*/
+
+
+
+
+
+
+
+
+
 
 
 
